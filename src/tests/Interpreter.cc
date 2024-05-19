d8796bcc091fce58bf988eb6e2ea7768475d9000
LiWinDom <wadim.gorbachev@mail.ru>
2024-05-19 13:00:39 +0300

    Windows bugfix

--- src/tests/Interpreter.cc
+++ src/tests/Interpreter.cc
@@ -1,12 +1,26 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include <stack>
-
 #include "../Interpreter.cpp"
-#include "Mock_TerminalExpression_Minus.h"
 #include "Mock_TerminalExpression_Number.h"
 #include "Mock_TerminalExpression_Plus.h"
+#include "Mock_TerminalExpression_Minus.h"
+
+TEST(TerminalExpression_Number, Mock) {
+  Mock_TerminalExpression_Number obj(0);
+  std::stack<int> s;
+
+  EXPECT_CALL(obj, Interpret(s)).Times(1);
+  obj.Interpret(s);
+}
+
+TEST(TerminalExpression_Plus, Mock) {
+  Mock_TerminalExpression_Plus obj;
+  std::stack<int> s;
+
+  EXPECT_CALL(obj, Interpret(s)).Times(1);
+  obj.Interpret(s);
+}
 
 TEST(TerminalExpression_Minus, Mock) {
   Mock_TerminalExpression_Minus obj;
